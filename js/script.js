87cc83f10da12235710425d62e72bedfd6c946cf
sannasinne <sanna@keistinen.fi>
2024-01-29 13:49:22 +0200

    Added click event

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
-const statusButton = document.querySelector("button");
+const statusButton = document.querySelector(
+  "button"
+);
 const pets = document.querySelector(".all-pets");
 
 const createPet = function (name, species) {
   const pet = {
     name: name,
     species: species,
-    isTired: 5,
+    isTired: 5, // Scale from 1 (refreshed) to 10 (exhausted)
     sleep: function () {
-      console.log(`${this.name} needs a nap. Zzz…`);
+      console.log(
+        `${this.name} needs nap. Zzz...`
+      );
       this.isTired = 1;
     },
     play: function () {
@@ -15,11 +19,14 @@ const createPet = function (name, species) {
         console.log("Too tired to play.");
         this.sleep();
       } else {
-        console.log(`Yay! ${this.name} loves to play!`);
+        console.log(
+          `Yay! ${this.name} loves to play!`
+        );
         this.isTired += 1;
       }
     }
   };
+
   return pet;
 };
 
@@ -39,16 +46,23 @@ const francine = createPet("Francine", "turtle");
 clover.isTired = 8;
 francine.isTired = 9;
 
-const allPets = [sora, clover, baxter, cleo, francine];
+const allPets = [
+  sora,
+  clover,
+  baxter,
+  cleo,
+  francine
+];
 
-console.log(allPets);
+// console.log(allPets);
 
 const showPets = function (petArray) {
+
   pets.innerHTML = "";
 
   for (let pet of petArray) {
     let status = "ready to play!";
-    if (isTired >= 7) {
+    if (pet.isTired >= 7) {
       status = "sleeping.";
     }
     const li = document.createElement("li");
@@ -56,3 +70,8 @@ const showPets = function (petArray) {
     pets.append(li);
   }
 };
+
+statusButton.addEventListener("click", function () {
+    showPets(allPets);
+  }
+);
