452f452795c77a59acf45de5fd7413e05ab869a2
sannasinne <sanna@keistinen.fi>
2024-01-29 13:42:06 +0200

    Displaying pets in the browser

--- js/script.js
+++ js/script.js
@@ -42,3 +42,17 @@ francine.isTired = 9;
 const allPets = [sora, clover, baxter, cleo, francine];
 
 console.log(allPets);
+
+const showPets = function (petArray) {
+  pets.innerHTML = "";
+
+  for (let pet of petArray) {
+    let status = "ready to play!";
+    if (isTired >= 7) {
+      status = "sleeping.";
+    }
+    const li = document.createElement("li");
+    li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}`;
+    pets.append(li);
+  }
+};
