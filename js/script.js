2f0905247fb6bcd3a7737c8cab7c4ea917251097
sannasinne <sanna@keistinen.fi>
2024-01-29 13:04:28 +0200

    Created a factory function and added properties

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 const statusButton = document.querySelector("button");
 const pets = document.querySelector(".all-pets");
+
+const createPet = function (name, species) {
+  const pet = {
+    name: name,
+    species: species,
+    isTired: 5
+  }
+};
